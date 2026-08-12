ea1855e95513968e97d226bc94782adcdd365f10
xytyymq <83972451@qq.com>
2026-08-13 01:58:40 +0800

    fix: sw network-first + auto-reload, regenerate index

--- badminton-selfstudy/sw.js
+++ badminton-selfstudy/sw.js
@@ -1,11 +1,12 @@
-const CACHE='xingyi-badminton-v1';
-const ASSETS=['./index.html','./manifest.webmanifest','./icon.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET')return;
-  e.respondWith(caches.match(e.request).then(r=>{
-    if(r)return r;
-    return fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp;}).catch(()=>caches.match('./index.html'));
-  }));
+const CACHE="xingyi-badminton-v2";
+const ASSETS=["./index.html","./manifest.webmanifest","./icon.png"];
+self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener("fetch",function(e){
+  if(e.request.method!=="GET")return;
+  e.respondWith(fetch(e.request).then(function(resp){
+    var cp=resp.clone();
+    caches.open(CACHE).then(function(c){c.put(e.request,cp);});
+    return resp;
+  }).catch(function(){return caches.match(e.request).then(function(r){return r||caches.match("./index.html");});}));
 });
